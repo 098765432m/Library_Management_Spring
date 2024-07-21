4c771b64bb6ab698eb4f363ec719ce2d60b46119
098765432m <anphutran73@gmail.com>
2024-07-21 13:03:41 +0700

    Initialize commit: Set up project

---
+++ Library_Management_Spring_BackEnd/src/main/java/com/project_1/Library_Management_Spring_BackEnd/LibraryManagementSpringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project_1.Library_Management_Spring_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSpringBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSpringBackEndApplication.class, args);
+	}
+
+}
---
+++ Library_Management_Spring_BackEnd/src/test/java/com/project_1/Library_Management_Spring_BackEnd/LibraryManagementSpringBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_1.Library_Management_Spring_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSpringBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
